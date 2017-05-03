2f073ceda6e251a8de37d212b2f0c78c87e6d5b7
robsonprod <rbsazevedo@gmail.com>
2017-05-03 08:47:07 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Equipe Deai Karate Shotokan</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/swiper.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link rel="icon" type="image" href="assets/img/Logotipo-DEAI-1.jpg">
+
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+  <header>
+    <nav class="navbar navbar-default navbar-fixed-top" style="box-shadow: 0 20px 12px 0 rgba(54, 53, 53, 0.11);">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+          <a class="navbar-brand logoDeai" href="#myPage">
+            <img src="assets/img/Logotipo-DEAI-1.jpg" alt="deai" data-os-animation="fadeIn" data-os-animation-delay="1s">
+          </a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#" data-target="#shotokan" onclick="$(this).autoScroll(); return false;">Home</a></li>
+            <li><a href="#" data-target="#sobre" onclick="$(this).autoScroll(); return false;">Quem somos</a></li>
+            <li><a href="#" data-target="#fotos" onclick="$(this).autoScroll(); return false;">Fotos</a></li>
+            <li><a href="#" data-target="#Patrocinadores" onclick="$(this).autoScroll(); return false;">Patrocinadores</a></li>
+            <li><a href="#" data-target="#contato" onclick="$(this).autoScroll(); return false;">Contato</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <div class="container">
+      <dir class="row">
+        <img src="assets/img/imagem1.jpg" class="fullscreen">
+      </dir>
+    </div>
+  </main>
+
+<!-- Container (About Section) -->
+<div id="shotokan" class="container-fluid bg-grey2">
+
+  <div class="row">
+    <div class="col-sm-8">
+      <h1 class="text-primary bordered">Sh<span style="text-decoration: underline;">oto</span>kan</h1><br>
+      <h4 class="text-justify">Shotokan (松涛館?) é um dos estilos de karatê que surgiu dos ensinamentos ministrados pelo mestre Gichin Funakoshi e por seu filho, Yoshitaka Funakoshi[a]. O repertório técnico do estilo foi baseado no do Shorin-ryu, mas, devido aos estudos empreendido pelo filho do mestre e sua influência, várias técnicas foram incorporadas e/ou modificadas, de modo a refletir o escopo almejado, que era o de valorizar mais o lado desportivo e físico como forma de promover o desenvolvimento pessoal.
+        Mestre Funakoshi em princípio não denominou o que ele ensinava de um estilo próprio, mas, antes de tudo, afirmava que ensinava karatê. Por outro lado, é certo que ele ensinava a arte marcial de acordo com sua visão e entendimento particulares sobre a mesma, mas isso seria explicado — também segundo o próprio mestre comentava — como uma consequência natural, pois vários professores ensinariam uma mesma disciplina de modos diferentes. Entretanto, alguns de seus alunos, como forma de o homenagear, manufaturaram uma placa com a inscrição Shotokan, eis que Shoto era a alcunha que o mestre assinava suas obras, pelo que o dojô passou a ser conhecido como "casa de Shoto".[1]
+        A despeito de outros mestres tentarem antes e contemporaneamente, foi o estilo de mestre Funakoshi que logrou êxito em vencer as barreiras culturais opostas a Oquinaua e difundir o caratê pelo resto do Japão, modificando nomes de técnicas e adaptando outras.
+        O Shotokan foi totalmente baseado no estilo Shorin-ryu,tanto que o criador era praticante desse estilo.O Shotokan foi criado para a correção de alguns detalhes e ocultar a influência chinesa por conta da rivalidade entre o Japão e a China, para ser bem recebido pela divulgação em Tóquio em 1921.
+      </h4>
+    </div>
+    <div class="col-sm-4">
+      <span class="logo"><img src="assets/img/shotokan tiger.png" style="width: 100%;"></span>
+    </div>
+  </div>
+</div>
+
+<div id="sobre" class="container-fluid">
+  <div class="row">
+    <div class="col-sm-4">
+      <span class="logo slideanim"><img src="assets/img/logomarca.jpg" style="width: 100%;"></span>
+    </div>
+    <div class="col-sm-8">
+      <h1 class="text-primary">Quem So<span style="text-decoration: underline;">mos<span></h1><br>
+      <h4><strong>MISSION:</strong> Our mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
+      <p><strong>VISION:</strong> Our vision Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  </div>
+</div>
+
+
+<!-- Container (Portfolio Section) -->
+<div id="fotos" class="container-fluid text-center bg-grey2">
+  <h1 class="text-primary">Gale<span style="text-decoration: underline;">ria </span>de Fotos</h1><br>
+
+  <!-- Slider main container -->
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide"><img src="assets/img/campeonato3.jpg"></div>
+      <div class="swiper-slide"><img src="assets/img/XXIIcampcearense1.jpg"></div>
+      <div class="swiper-slide"><img src="assets/img/17239812_1664562333847544_4244170614867332590_o.jpg"></div>
+      <div class="swiper-slide"><img src="assets/img/XXIIcampcearense1.jpg"></div>
+      <div class="swiper-slide"><img src="assets/img/XXIIcampcearense2.jpg"></div>
+      <div class="swiper-slide"><img src="assets/img/XXIIcampcearense3.jpg"></div>
+    </div>
+    <!-- Add Arrows -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+
+    <div class="swiper-pagination"></div>
+  </div>
+  
+  
+</div>
+
+<div id="Patrocinadores" class="container-fluid">
+  <div class="row">
+    <div class="col-sm-12 text-center">
+    <h1 class="text-primary slideanim">P<span style="text-decoration: underline;">atro</span>cinadores</h1>
+      <a href="http://www.faststop.com.br" target="_blank">
+        <span>
+          <img src="assets/img/faststop.jpg" class="slideanim">
+        </span>
+      </a>
+      
+    </div>
+  </div>
+
+<!-- Add Google Maps -->
+  <div id="googleMap" class="center" style="height:400px;width:100%;"></div>
+</div>
+<script>
+function myMap() {
+    var myCenter = new google.maps.LatLng(-3.803960, -38.521968);
+    var mapProp = {center:myCenter, zoom:15, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var marker = new google.maps.Marker({position:myCenter});
+    marker.setMap(map);
+}
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQc5Qa0i8YG4Kcm8WBvGct9XffGerPWVQ&callback=myMap"></script>
+
+<!-- Container (Contact Section) -->
+<div id="contato" class="container-fluid bg-grey2">
+  <h1 class="text-center text-primary">Con<span style="text-decoration: underline;">tat</span>o</h1>
+  <div class="row">
+    <div class="form-group col-md-6 col-sm-6 form-group text-center slideanim">
+      <p></p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> Fortaleza, CE</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> Av. Alberto Craveiro, 2936</p>
+      <p><span class="glyphicon glyphicon-earphone"></span> (85) 00000-0000</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> email@gmail.com</p>
+      <div class="fb-page" data-href="https://www.facebook.com/equipedeai" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false">
+        <div class="fb-xfbml-parse-ignore">
+          <blockquote cite="https://www.facebook.com/equipedeai"><a href="https://www.facebook.com/equipedeai">Deai</a></blockquote>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-sm-6 slideanim">
+      <div id="result"><h4 class="text-center"></h4></div>
+      <p>Deixe sua mensagem, nós retornaremos o mais breve possível, críticas construtivas tabém são bem vindas.</p>
+      <div class="form-group">
+        <input class="form-control" id="name" name="name" placeholder="Nome" type="text" required>
+      </div>
+      <div class="form-group">
+        <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+      </div>
+
+      <textarea class="form-control" id="message" name="message" placeholder="Menssagem" rows="5"></textarea>
+    </div>
+
+    <div class="col-md-12 col-sm-12 form-group">
+      <button class="btn btn-success pull-right" type="submit"><span class="glyphicon glyphicon-ok"></span> Enviar</button>
+    </div>
+  </div>
+</div>
+
+
+<footer class="myFooter text-center" style="background-color: #2f2f2f">
+  <a href="#myPage" title="To Top" style="float: right; padding: 30px 30px">
+    <span class="glyphicon glyphicon-chevron-up apresentacao"></span>
+  </a>
+  <div class="container">
+    <div class="row" >
+    <div class="col-md-12">
+      <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <h4 style="color: #fff">"Não importa onde você caiu ou em que momento da vida você cansou, o importante é sempre levantar e tentar novamente."<br><span>Equipe Deai</span></h4>
+      </div>
+      <div class="item">
+        <h4 style="color: #fff">"A vida pode te derrubar, mas você decide quando é hora de levantar."<br><span>Karate kid</span></h4>
+      </div>
+      <div class="item">
+        <h4 style="color: #fff">"O treinamento não é um caminho florido; é rotina repetitiva e simples. Suportar essa rotina é a coisa mais importante, é o meio de adquirir a confiança em si, a base do autocontrole, para então adquirir uma total e perfeita serenidade de espírito."<br><span>Shihan Seiji Isobe
+</span></h4>
+      </div>
+      <div class="item">
+        <h4 style="color: #fff">"A vida nos leva para baixo, mas nós podemos escolher se vamos ou não levantar-nos."
+          <br>
+          <span>Karate kid 4</span>
+        </h4>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+    </div>
+      <div class="col-md-12 text-center" style="padding-top: 30px;">
+        <p style="color: #fff;">desenvolvido por <br class="visible-xs"><a href="http://www.includetecnologia.com.br">Include tecnologia</a></p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+<!-- Modal -->
+<div class="modal fade" id="aberturaModal" tabindex="-1" role="dialog" aria-labelledby="aberturaModalLabel" style="margin-top: 100px;">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <!-- <div class="modal-header"> -->
+        <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> -->
+        <!-- <h4 class="modal-title" id="aberturaModalLabel"></h4> -->
+      <!-- </div> -->
+      <div class="modal-body">
+        <div class="thumbnail text-center modalEvento">
+          <div class="">
+            <h3 class="text-primary">Próximo Evento</h3>
+            <br><br>
+            <p>FCKI – 2o Treino para Zonal Norte Nordeste</p>
+            <p>Quando: Sábado, 04 de Março de 2017 das 15h às 17h</p>
+            <p>Local: Colégio Maria Ester I</p>
+            <p>Rua Antônio Teixeira Leite, 248</p>
+            <p>Itaperi, Fortaleza – CE, Brasil</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- fan page -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.8&appId=1186020861462507";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+<script type="text/javascript" src="assets/js/jquery-3.0.0.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/swiper.jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/swiper.min.js"></script>
+<script>
+  $.fn.autoScroll = function() {
+    var target = $(this).data('target');
+    $("html, body").animate({ scrollTop: $(target).offset().top }, 1000);
+  };
+
+  $(document).ready(function(){
+  $('#aberturaModal').modal('show');
+
+
+  var swiper = new Swiper('.swiper-container', {
+    paginationClickable: true,
+    pagination: '.swiper-pagination',
+    effect: 'flip',
+    grabCursor: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev'
+  });
+
+  $("#myPage").niceScroll({styler:"fb",cursorcolor:"#000"});
+
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+
+       window.location.hash = hash;
+     });
+    }
+  });
+
+  $(window).scroll(function() {
+   $(".slideanim").each(function(){
+     var pos = $(this).offset().top;
+
+     var winTop = $(window).scrollTop();
+     if (pos < winTop + 600) {
+       $(this).addClass("slide");
+     }
+   });
+ });
+
+  jQuery('#contactForm').submit(function(){
+    var dados = jQuery( this ).serialize();
+
+    jQuery.ajax({
+      type: "POST",
+      url: "enviar.php",
+      data: dados,
+      datatype: 'json',
+      success: function(data){
+        $('#result').html(data.success +'' + data.message);   
+        $("#result").addClass('alerta alerta-success');
+        $("#result").fadeIn(1500);
+        setTimeout(function() {
+          $("#result").remove(); 
+        }, 6000);
+      },
+    });
+    return false;
+    e.preventDefault();
+  });
+
+})
+</script>
+    </body>
+</html>
+
